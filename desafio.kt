95f1f6e6437656561f471961dbdcb03772fb5f7b
Anahita Monezi <109434062+NallyNik@users.noreply.github.com>
2022-12-02 17:14:15 -0300

    Completando desafio

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,44 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario (val id: Int, val nome: String, val sobrenome: String, val idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: String) {
+    companion object Conteudo {
+        val listaConteudo = mutableListOf<ConteudoEducacional>()
+        fun addConteudo (conteudo: ConteudoEducacional) {
+            listaConteudo.add(conteudo)
+        }
+    }
+}
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    companion object Lista {
+        val inscritos = mutableListOf<Usuario>()
+        fun matricular(usuario: Usuario) {
+        	inscritos.add(usuario)
+    	}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val inteligenciaArtificial = ConteudoEducacional("inteligencia artificial", 500, "DIFICIL")
+    val softSkills = ConteudoEducacional("soft skills", 100, "BASICO")
+
+    val aluno1:Usuario = Usuario(1, "Adolpho", "Silva", 25)
+    val aluno2 = Usuario(2, "Alice", "Maravilhas", 39)
+    val aluno3 = Usuario(3, "Branco", "Coelho", 50)
+    
+	ConteudoEducacional.addConteudo(inteligenciaArtificial)
+    ConteudoEducacional.addConteudo(softSkills)
+    
+    Formacao.matricular(aluno1)
+    Formacao.matricular(aluno2)
+    Formacao.matricular(aluno3)
+        
+    println(aluno1)
+    println(aluno2.nome)
+    println(softSkills)
+    println(ConteudoEducacional.listaConteudo)
+    println(Formacao.inscritos)
 }
